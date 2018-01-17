d573a635d03374b17014adbfc2b7f5a23fd4c066
Soham Patel <sapcambay95@gmail.com>
2018-01-17 10:48:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,10 +34,38 @@ def makeWebhookResult(req):
     components = parameters.get("components")
     emergency = parameters.get("emergency")
 
+    emergency_dict = {
+    'Heart Attack':'Heart attack is very severe', 
+    'Paralysis':'Paralysis is very severe', 
+    'Snake Bite':'Snake bite is very severe', 
+    'Disease4':400, 
+    'Disease5':500
+    }
+
+    precaution_dict = {
+    'Heart Attack':'1. HAP1.~2. HAP2.', 
+    'Paralysis':'1. PP1.~2. PP2', 
+    'Snake Bite':'1.SBP1.~2.SBP2.', 
+    'Disease4':400, 
+    'Disease5':500
+    }
+
+    symptom_dict = {
+    'Heart Attack':'1. HAS1.~2. HAS2.', 
+    'Paralysis':'1. PS1.~2. PS2.', 
+    'Snake Bite':'1. SBS1.~2. SBS2.', 
+    'Disease4':400, 
+    'Disease5':500
+    }
+
     if(components == ""):
-        speech = str("done")
-    else:
-        speech = str(components) + str(" webhook ") + str(emergency)
+        speech = str(emergency_dict[emergency])
+    else if(components == "Precaution"):
+        speech = str(precaution_dict[emergency])
+    else if(components == "Symptom"):
+        speech = str(symptom_dict[emergency])
+    else
+        speech = str("not found")
 
     print("Response:")
     print(speech)
