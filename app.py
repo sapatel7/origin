d3c43c34dd5740b1dd0f74e4462c68eb967d2db9
Soham Patel <sapcambay95@gmail.com>
2018-01-17 11:50:15 +0530

    Final Content Update

--- app.py
+++ app.py
@@ -36,9 +36,9 @@ def makeWebhookResult(req):
 
     emergency_dict = {"Heart Attack":"Heart attack is very severe", "Paralysis":"Paralysis is very severe", "Snake Bite":"Snake bite is very severe", "Disease4":"s", "Disease5":"s"}
 
-    precaution_dict = {"Heart Attack":"~1. HAP1.~2. HAP2", "Paralysis":"~1. PP1.~2. PP2", "~1. SNP1.~2. SNP2", "Disease4":"s", "Disease5":"s"}
+    precaution_dict = {"Heart Attack":"~1. HAP1.~2. HAP2", "Paralysis":"~1. PP1.~2. PP2", "Snake Bite":"~1. SNP1.~2. SNP2", "Disease4":"s", "Disease5":"s"}
 
-    symptom_dict = {"Heart Attack":"~1. HAS1.~2. HAS2", "Paralysis":"~1. PS1.~2. PS2", "~1. SNS1.~2. SNS2", "Disease4":"s", "Disease5":"s"}
+    symptom_dict = {"Heart Attack":"~1. HAS1.~2. HAS2", "Paralysis":"~1. PS1.~2. PS2", "Snake Bite":"~1. SNS1.~2. SNS2", "Disease4":"s", "Disease5":"s"}
 
     if(components == ""):
         speech = str(emergency_dict[emergency])
